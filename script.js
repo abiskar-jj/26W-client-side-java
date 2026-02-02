1ceda3bce35503d3955156966b50806685b2cd7f
abiskar-jj <abiskarchhetri86@gmail.com>
2026-02-01 23:14:01 -0500

    initialzed variable and added arrays to store data to make random description

---
+++ script.js
@@ -0,0 +1,24 @@
+let petName = "Luna";
+let petType = "dragon";
+let petAge = 3;
+let isHungry = false;
+let mood = "excited";
+let favoriteActivity = "flying";
+
+const ageText = document.getElementById("ageText");
+const hungryText = document.getElementById("hungryText");
+const description = document.getElementById("description");
+
+const randomBtn = document.getElementById("randomBtn");
+const ageUp = document.getElementById("ageUp");
+const ageDown = document.getElementById("ageDown");
+
+const nameInput = document.getElementById("nameInput");
+const nameBtn = document.getElementById("nameBtn");
+const hungryCheck = document.getElementById("hungryCheck");
+
+const petTypes = ["dog", "cat", "dragon", "hamster"];
+const moods = ["happy", "sleepy", "excited", "grumpy"];
+const activities = ["flying", "playing", "napping", "running"];
+
+
