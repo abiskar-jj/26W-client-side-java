4c0f1ce4469e851436679089870d59d1b84a0ef5
abiskar-jj <abiskarchhetri86@gmail.com>
2026-02-16 13:44:54 -0500

    Create function to update background color using RGB values

--- lab-2/js/page-color-changer.js
+++ lab-2/js/page-color-changer.js
@@ -6,6 +6,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const greenSlider = document.getElementById("green");
   const blueSlider = document.getElementById("blue");
 
+  // HTML element whose background will change
+  const htmlElement = document.documentElement;
+
+  // Function to update background color
+  function updateBackgroundColor() {
+    const r = Number(redSlider.value);
+    const g = Number(greenSlider.value);
+    const b = Number(blueSlider.value);
+
+    htmlElement.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  }
 
 
 
