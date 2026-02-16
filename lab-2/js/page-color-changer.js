439008e9d9696d9a70649ffbcbfbbab00e3e951b
abiskar-jj <abiskarchhetri86@gmail.com>
2026-02-16 13:14:47 -0500

    Add references to RGB slider elements

--- lab-2/js/page-color-changer.js
+++ lab-2/js/page-color-changer.js
@@ -1,5 +1,14 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+     // Get slider elements
+  const redSlider = document.getElementById("red");
+  const greenSlider = document.getElementById("green");
+  const blueSlider = document.getElementById("blue");
+
+
+
+
 });
 
+ 
